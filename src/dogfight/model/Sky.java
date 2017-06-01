37a54933324246fde75f991869f1ea11fae34e24
Robin Maisano <robin.maisano@gmail.com>
2017-06-01 16:25:02 +0200

    Travail terminé;

---
+++ src/dogfight/model/Sky.java
@@ -0,0 +1,24 @@
+package dogfight.model;
+
+import java.awt.Image;
+import dogfight.model.Dimension;
+
+public class Sky implements IArea{
+	
+	private Dimension dimension;
+	
+	
+	
+	public Sky(Dimension dimension){
+		Image image = new Image();
+	}
+	
+	public Dimension getDimension(){
+		return this.dimension;
+	}
+	
+	public Image getImage(){
+		return <Image>;
+	}
+	
+}
